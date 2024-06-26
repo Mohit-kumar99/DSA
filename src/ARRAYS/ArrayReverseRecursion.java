ec985a7496e138b8edccaf30786d7acc003e6b82
Mohit <mohitkumar995318@gmail.com>
2024-06-27 01:03:21 +0530

    dsa first commit
    Signed-off-by: Mohit <mohitkumar995318@gmail.com>

---
+++ src/ARRAYS/ArrayReverseRecursion.java
@@ -0,0 +1,27 @@
+package ARRAYS;
+//ArrayReverseRecursion
+public class ArrayReverseRecursion {
+    static void reverseArray(int arr[], int start, int end){
+        int temp;
+        if(start>=end)
+            return;
+        temp = arr[start];
+        arr[start]= arr[end];
+        arr[end]= temp;
+        reverseArray(arr, +1, -1);
+
+    }
+    static void printArray(int arr[],int size){
+        for (int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        int arr[] ={1,2,3,4,5,6};
+        printArray(arr,6);
+        reverseArray(arr, 0, 5);
+        System.out.println("Reversed array is ");
+        printArray(arr, 6);
+    }
+}
